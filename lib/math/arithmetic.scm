c0d8b04bd4ab35a08b3091f40094b71ec14aef28
Cian Hughes <chughes000@gmail.com>
2025-12-12 17:02:16 +0000

    Added factorial function to library

--- lib/math/arithmetic.scm
+++ lib/math/arithmetic.scm
@@ -3,3 +3,12 @@
 
 (define (square x)
   (* x x))
+
+(define (factorial n)
+  (define (iter product counter)
+    (if (> counter n)
+      product
+      (iter (* counter product)
+        (+ counter 1))))
+
+  (iter 1 1))
