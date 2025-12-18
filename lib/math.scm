efedfc60e5c66d8e701018b5ad5664dc0f85d4be
Cian Hughes <chughes000@gmail.com>
2025-12-18 14:02:19 +0000

    Added Euclid's algorithm implementation for gcd

--- lib/math.scm
+++ lib/math.scm
@@ -1,4 +1,5 @@
 (load "lib/math/arithmetic.scm")
+(load "lib/math/factorisation.scm")
 
 (load "lib/math/sqrt.scm")
 (load "lib/math/cubert.scm")
---
+++ lib/math/factorisation.scm
@@ -0,0 +1,4 @@
+(define (gcd a b)
+  (if (zero? b)
+    a
+    (gcd b (remainder a b))))
