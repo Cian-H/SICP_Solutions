fb82d1c94537d2b5bf0f37cb1efcaf8968694a65
Cian Hughes <chughes000@gmail.com>
2025-12-18 14:04:05 +0000

    Refactored root algorithms in library

--- lib/math.scm
+++ lib/math.scm
@@ -1,5 +1,3 @@
 (load "lib/math/arithmetic.scm")
 (load "lib/math/factorisation.scm")
-
-(load "lib/math/sqrt.scm")
-(load "lib/math/cubert.scm")
+(load "lib/math/roots.scm")
--- lib/math/roots.scm
+++ lib/math/roots.scm
@@ -14,3 +14,16 @@
     (average guess (/ x guess)))
 
   (iter (improve 1.0)))
+
+(define (cubert x)
+
+  (define (iter guess)
+    (define next_guess (improve guess))
+    (if (>= (abs next_guess) (abs guess))
+      guess
+      (iter next_guess)))
+
+  (define (improve guess)
+    (/ (+ (/ x (square guess)) guess guess) 3))
+
+  (iter (improve 1.0)))
