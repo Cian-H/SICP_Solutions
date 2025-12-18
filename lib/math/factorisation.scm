1f5e69ac47016f28e30d732c21e5943fd80cb768
Cian Hughes <chughes000@gmail.com>
2025-12-18 14:17:36 +0000

    Added smallest-divisor and prime test to factorisation

--- lib/math/factorisation.scm
+++ lib/math/factorisation.scm
@@ -1,4 +1,20 @@
+(load "lib/math/arithmetic.scm")
+
 (define (gcd a b)
   (if (zero? b)
     a
     (gcd b (remainder a b))))
+
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond
+    ((> (square test-divisor) n) n)
+    ((divides? test-divisor n) test-divisor)
+    (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (zero? (remainder b a)))
+
+(define (prime?) n
+  (= n (smallest-divisor n)))
