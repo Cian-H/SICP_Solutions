bc4a6f98e99c111e4e92bdef9741735832011556
Cian Hughes <chughes000@gmail.com>
2025-12-18 12:19:57 +0000

    Added fast-mult to library (as study in custom impl)

--- lib/math/arithmetic.scm
+++ lib/math/arithmetic.scm
@@ -4,6 +4,16 @@
 (define (average x y)
   (/ (+ x y) 2))
 
+(define (fast-mult x n)
+
+  (define (iter b i a)
+    (cond
+      ((= i 0) a)
+      ((even? i) (iter (+ b b) (/ i 2) a))
+      (else (iter b (- i 1) (+ a b)))))
+
+  (iter x n 0))
+
 (define (square x)
   (* x x))
 
@@ -25,3 +35,13 @@
       (else (iter b (- i 1) (* a b)))))
 
   (iter x n 1))
+
+(define (* x n)
+
+  (define (iter b i a)
+    (cond
+      ((= i 0) a)
+      ((even? i) (iter (+ b b) (/ i 2) a))
+      (else (iter b (- i 1) (+ a b)))))
+
+  (iter x n 0))
