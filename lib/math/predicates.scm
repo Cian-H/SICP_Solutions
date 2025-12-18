087b93dbdb2fa3776dea6194ca57c4badaf5e0a4
Cian Hughes <chughes000@gmail.com>
2025-12-18 15:20:47 +0000

    Implemented primality tests

--- lib/math/predicates.scm
+++ lib/math/predicates.scm
@@ -4,5 +4,24 @@
 (define (even? n)
   (divides? 2 n))
 
-(define (prime?) n
+(define (prime? n)
   (= n (smallest-divisor n)))
+
+(define (fermat-prime? n times)
+
+  (define (expmod base exp m)
+    (cond
+      ((= exp 0) 1)
+      ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
+      (else (remainder (* base (expmod base (- exp 1) m)) m))))
+
+  (define (try-it a)
+    (= (expmod a n n) a))
+
+  (define (fermat-test n)
+    (try-it (+ 1 (random (- n 1)))))
+
+  (cond
+    ((zero? times) true)
+    ((fermat-test n) (fermat-prime? n (- times 1)))
+    (else false)))
