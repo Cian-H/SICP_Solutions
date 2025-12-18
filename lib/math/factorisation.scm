2115c43cd76760f3f2c8e147e95f64504a95300d
Cian Hughes <chughes000@gmail.com>
2025-12-18 18:40:52 +0000

    Implemented improvements from exercises into library

--- lib/math/factorisation.scm
+++ lib/math/factorisation.scm
@@ -5,8 +5,13 @@
 
 (define (smallest-divisor n) (find-divisor n 2))
 
+(define (next-divisor p)
+  (if (= p 2)
+    3
+    (+ p 2)))
+
 (define (find-divisor n test-divisor)
   (cond
     ((> (square test-divisor) n) n)
     ((divides? test-divisor n) test-divisor)
-    (else (find-divisor n (+ test-divisor 1)))))
+    (else (find-divisor n (next-divisor test-divisor)))))
--- lib/math/predicates.scm
+++ lib/math/predicates.scm
@@ -25,3 +25,38 @@
     ((zero? times) true)
     ((fermat-test n) (fermat-prime? n (- times 1)))
     (else false)))
+
+(define (mr-prime? n times)
+
+  (define (expmod base exp m)
+    (cond
+      ((= exp 0) 1)
+      ((even? exp
+          (let ((x (expmod base (/ exp 2) m)))
+            (if (= x 0)
+              0
+              (check-nontrivial-sqrt x m)))))
+      (else
+        (let ((x (expmod base (- exp 1) m)))
+          (if (= x 0)
+            0
+            (remainder (* base x) m))))))
+
+  (define (check-nontrivial-sqrt x m)
+    (let ((r (remainder (square x) m)))
+      (if (and (= r 1)
+           (not (= x 1))
+           (not (= x (- m 1))))
+        0
+        r)))
+
+  (define (try-it a)
+    (= (expmod a (- n 1) n) 1))
+
+  (define (miller-rabin-test n)
+    (try-it (+ 1 (random (- n 1)))))
+
+  (cond
+    ((= times 0) true)
+    ((miller-rabin-test n) (mr-prime? n (- times 1)))
+    (else false)))
