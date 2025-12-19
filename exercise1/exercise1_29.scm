1e7c2e6e1f991534b787cccde96009fd3768ed21
Cian Hughes <chughes000@gmail.com>
2025-12-19 01:26:06 +0000

    Solved exercise 1.29

---
+++ exercise1/exercise1_29.scm
@@ -0,0 +1,24 @@
+(load "lib/math.scm")
+
+(define (integral f a b n)
+
+  (define h (/ (- b a) n))
+
+  (define (series-prefix k)
+    (cond
+      ((or (zero? k) (= k n)) 1)
+      ((even? k) 2)
+      (else 4)))
+
+  (define (yk k)
+    (f (+ a (* k h))))
+
+  (define (series k)
+    (* (series-prefix k) (yk k)))
+
+  (define (next k)
+    (+ k 1))
+
+  (*
+    (/ h 3)
+    (sum series 0 next n)))
