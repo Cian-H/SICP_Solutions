0d7f8b416729488434681fe2d8d46b1fc5d20c03
Cian Hughes <chughes000@gmail.com>
2026-03-20 10:33:02 +0000

    Added type coercion system to generic dispatcher

--- lib/generic-dispatch.scm
+++ lib/generic-dispatch.scm
@@ -1,9 +1,28 @@
 (load "lib/safe-load.scm")
 (load "lib/dispatch-table.scm")
 
+(define (put-coercion t1 t2 proc)
+  (put 'coerce t1 t2 proc))
+
+(define (get-coercion t1 t2)
+  (get 'coerce t1 t2))
+
 (define (apply-generic op . args)
   (let* ((types (map type-of args))
          (proc (get op types)))
     (if proc
       (apply proc (map type-unwrap args))
-      (error "apply-generic: no method for types" (list op types)))))
+      (if (= (length args) 2)
+        (let* ((type1 (car types))
+               (type2 (cadr types))
+               (a1 (car args))
+               (a2 (cadr args)))
+          (if (eq? type1 type2)
+            (error "apply-generic: no method for types" (list op types))
+            (let ((t1->t2 (get-coercion type1 type2))
+                  (t2->t1 (get-coercion type2 type1)))
+              (cond
+                (t1->t2 (apply-generic op (t1->t2 a1) a2))
+                (t2->t1 (apply-generic op a1 (t2->t1 a2)))
+                (else (error "apply-generic: no method for types" (list op types)))))))
+        (error "apply-generic: no method for types" (list op types))))))
